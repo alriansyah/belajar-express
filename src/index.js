19f21273991d89f72c320ca1e59c7eea173bdc13
alriansyah <alriansyah1218@gmail.com>
2023-05-18 19:06:33 +0700

    belajar routing

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ const port = 3000;
 
 const app = express();
 
-app.listen(port, () => {
-    console.log(`Aplikasi berjalan dengan port : ${port} 😎`)
+app.get("/", (req, res) => {
+    res.send("<h1>Hello World.!");
 });
 
-app.get("/", (req, res) => {
-    res.send("Anjay berhasil");
+app.get("/perkenalan", (req, res) => {
+    res.send("<h1>Perkenelakan nama saya : Al Riansyah</h1>");
+});
+
+app.listen(port, () => {
+    console.log(`Aplikasi berjalan dengan port : ${port} 😎`)
 });
